86d804635e124ea3de7e7699cc6c35e86fe73e64
Mirko Bonadei <mirko.bonadei@gmail.com>
2015-08-11 18:37:00 +0200

    First draft of generated data collection
    With this feature we want to give the user a tool to understand which kind of data has been generated by the generators, so he/she will be able to tune them to maximize the efficiency of the quickcheck run on the given property.
    
    This is a draft commit, many things need to be polished.

--- src/Eris/Quantifier/ForAll.php
+++ src/Eris/Quantifier/ForAll.php
@@ -31,6 +31,8 @@ class ForAll
         'implies' => '__invoke',
         'imply' => '__invoke',
     ];
+    private $collectFn;
+    private $collectedValues;
 
     public function __construct(array $generators, $iterations, $shrinkerFactory)
     {
@@ -68,6 +70,13 @@ class ForAll
         return $this;
     }
 
+    public function collect(callable $collectFn)
+    {
+        $this->collectFn = $collectFn;
+        $this->collectedValues = [];
+        return $this;
+    }
+
     public function __invoke(callable $assertion)
     {
         $sizes = $this->sizes($this->maxSize);
@@ -79,6 +88,9 @@ class ForAll
                     $value = $generator($sizes[$currentSizeIndex]);
                     $values[] = $value;
                 }
+
+                $this->updateDataDistribution($values);
+
                 if (!$this->antecedentsAreSatisfied($values)) {
                     continue;
                 }
@@ -96,6 +108,7 @@ class ForAll
                     })
                     ->execute();
             }
+            $this->printDataDistribution();
         } catch (Exception $e) {
             $wrap = (bool) getenv('ERIS_ORIGINAL_INPUT');
             if ($wrap) {
@@ -208,4 +221,29 @@ class ForAll
             return ($n * ($n + 1)) / 2;
         };
     }
+
+    private function updateDataDistribution(array $values)
+    {
+        if (!is_null($this->collectFn)) {
+            $key = call_user_func_array($this->collectFn, $values);
+            if (array_key_exists($key, $this->collectedValues)) {
+                $this->collectedValues[$key]++;
+            } else {
+                $this->collectedValues[$key] = 1;
+            }
+        }
+    }
+
+    private function printDataDistribution()
+    {
+        if (!is_null($this->collectFn)) {
+            arsort($this->collectedValues, SORT_NUMERIC);
+            echo PHP_EOL;
+            foreach ($this->collectedValues as $key => $value) {
+                $probability = round(($value / $this->evaluations) * 100, 2);
+                echo  "{$probability}%  $key" . PHP_EOL;
+            }
+        }
+    }
+
 }
